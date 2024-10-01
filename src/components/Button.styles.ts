61f36290039b8e1e0d67e6bde064183fd909357c
Cassiano26 <candidocassiano2@gmail.com>
2024-10-01 17:44:23 -0300

    Inserindo estilos globais e implementando eslint.

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,10 +1,29 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success';
 
 interface ButtonContainerProps {
-  variant: 'primary' | 'secondary' | 'danger' | 'success';
-}
+  variant: ButtonVariant
+};
 
-export const ButtonContainer = styled.button`
+const buttonVariats = {
+  primary: 'purple',
+  secondary: 'orange',
+  danger: 'red',
+  success: 'green'
+};
+
+export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
-  height: 100px;
-` 
+  height: 40px;
+  border-radius: 0.25rem;
+  border: 0;
+  margin: 0.5rem;
+
+  background-color: ${props => props.theme['green-500']};
+  color: ${props => props.theme.white};
+
+  /* ${props => {
+    return css`background: ${buttonVariats[props.variant]}`
+  }} */
+`
